60032f9d17e6ad2e45f14466ce289bb4831aab7e
Shashank Tiwari <shashanktiwari3509@gmail.com>
2024-07-11 10:49:41 +0530

    Connection established

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-const express = reqire('express');
+const express = require("express");
+const connectToMongo = require("./db");
+const app = express();
+var cookieParser = require("cookie-parser");
+var cors = require("cors");
+
+connectToMongo();
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.listen(5002);
