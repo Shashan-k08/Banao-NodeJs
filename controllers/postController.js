dc256f636eba84b60b48137f7e800f3267c8773c
Shashank Tiwari <shashanktiwari3509@gmail.com>
2024-07-17 16:16:52 +0530

    like and comment  operations added

--- controllers/postController.js
+++ controllers/postController.js
@@ -47,3 +47,30 @@ const delete_post = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+const like_post = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+
+    post.likes++;
+    await post.save();
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const add_cmt = async (req, res) => {
+  const { user, content } = req.body;
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ message: "Post not Found !" });
+
+    post.comments.push({ user, content });
+    await post.save();
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
