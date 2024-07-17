385374258fd2f21e1a2566b3ba4bcd58eb7e55d5
Shashank Tiwari <shashanktiwari3509@gmail.com>
2024-07-17 16:10:15 +0530

    CRUD operations added

---
+++ controllers/postController.js
@@ -0,0 +1,49 @@
+const Post = require("../models/Post");
+
+const create_post = async (req, res) => {
+  const { user, content } = req.body;
+  const post = new Post({ user, content });
+  try {
+    const savedPost = await post.save();
+    res.status(201).json(savedPost);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const read_post = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ message: "Post not found" });
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const update_post = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ message: "Post not found !" });
+
+    if (req.body.content != null) {
+      post.content = req.body.content;
+    }
+
+    const updatedPost = await post.save();
+    res.json(updatedPost);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const delete_post = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json({ message: "Post not found !" });
+    await post.remove();
+    res.status(200).json({ message: "Post Deleted !" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
